32a27a48cb142471e1c955cca515fc40d73b5db3
Krzysztof Kin <sirex0@gmail.com>
2018-06-17 21:50:01 +0200

    menu and intent to activity

--- app/src/main/java/cloud/krzysztofkin/newsapp/NewsActivity.java
+++ app/src/main/java/cloud/krzysztofkin/newsapp/NewsActivity.java
@@ -10,6 +10,8 @@ import android.net.NetworkInfo;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -73,6 +75,26 @@ public class NewsActivity extends AppCompatActivity implements LoaderCallbacks<L
             errorMessageView.setText(R.string.no_internet);
         }
 
+    }
+
+    @Override
+    //  Initialize activity options menu
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the Options Menu we specified in XML
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    //select menu item
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
